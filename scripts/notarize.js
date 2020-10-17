fcaf562db52cb6cfc9ce9bbc6703520ff7b5ca5b
Mikael Rinnetmäki <mikael@sensotrend.com>
2020-10-17 19:43:38 +0300

    Properly describe the built app as a dev version

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -9,7 +9,7 @@ exports.default = async function notarizing(context) {
   const appName = context.packager.appInfo.productFilename;
 
   return await notarize({
-    appBundleId: 'com.sensotrend.Uploader',
+    appBundleId: 'com.sensotrend.UploaderDev',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
